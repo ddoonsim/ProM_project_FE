1028afb517450d5537693524cd06b2649a2b4578
ddoonsim <2sujg97@naver.com>
2024-02-13 16:23:15 +0900

    회원가입 폼 에러메세지 추가, 버튼 그룹 스타일 추가

--- src/langs/ko/validation.js
+++ src/langs/ko/validation.js
@@ -1,6 +1,10 @@
 const validation = {
   NotBlank_email: '⚠️이메일을 입력하세요.',
   NotBlank_password: '⚠️비밀번호를 입력하세요.',
+  NotBlank_confirmPassword: '⚠️비밀번호를 확인하세요.',
+  NotBlank_name: '⚠️이름을 입력하세요.',
+  NotBlank_mobile: '⚠️전화번호를 입력하세요.',
+  AssertTrue_join_agree: '⚠️가입 약관에 동의하세요.',
 };
 
 export default validation;
