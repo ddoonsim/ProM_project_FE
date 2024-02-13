3d2410341c0a92d6b0efd66a8389a4d8aea4ade9
ddoonsim <2sujg97@naver.com>
2024-02-13 17:23:52 +0900

    로그인 페이지 푸터 살림 ㅎㅎㅎㅎ

--- src/outlines/front/Footer.js
+++ src/outlines/front/Footer.js
@@ -1,13 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 // 푸터 스타일시트 설정
-const OuterBox = styled.footer``;
+const OuterBox = styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100px;
+  background: #fff;
+  z-index: 1;
+`;
 
 // 푸터
 const Footer = () => {
-    return (
-        <OuterBox>푸터</OuterBox>
-    );
+  return <OuterBox>푸터</OuterBox>;
 };
 
 export default Footer;
