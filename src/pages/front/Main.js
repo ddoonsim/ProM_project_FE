3ea479f1f7bce910215e987e738bd4bfd2deec1b
ddoonsim <2sujg97@naver.com>
2024-02-14 17:36:46 +0900

    로그인 시 홈 페이지 사진 X

--- src/pages/front/Main.js
+++ src/pages/front/Main.js
@@ -1,27 +1,25 @@
-import LoginContainer from '../../containers/member/LoginContainer';
+import home_ex from '../../images/home_ex.png';
+import styled from 'styled-components';
 import UserContext from '../../modules/user';
 import { useContext } from 'react';
+
+const Img = styled.div`
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+`;
 
 const Main = () => {
   const {
     state: { isLogin, userInfo },
-    action: { setIsLogin, setUserInfo },
   } = useContext(UserContext);
 
-  const handleClick = () => {
-    setIsLogin(true);
-    setUserInfo({ userNm: '이이름' });
-  };
+  console.log('userInfo', { userInfo });
 
-  return isLogin ? (
-    <h1>메인페이지... / {userInfo.userNm}</h1>
-  ) : (
-    <>
-      <LoginContainer />
-      <button type="button" onClick={handleClick}>
-        로그인 하기
-      </button>
-    </>
+  return isLogin ? (<h1>{userInfo.name}님 로그인 성공 페이지 빠밤</h1>) : (
+    <Img>
+      <img src={home_ex} width="100%" alt="home" />
+    </Img>
   );
 };
 
