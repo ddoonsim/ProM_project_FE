99c877029ab4195f271e1c69fea627e51c52f93c
ddoonsim <2sujg97@naver.com>
2024-02-14 10:17:02 +0900

    자동 하이픈 추가

--- src/components/commons/ErrorMsgStyle.js
+++ src/components/commons/ErrorMsgStyle.js
@@ -6,8 +6,6 @@ const { primary } = colorNames;
 const { normal } = sizeNames;
 
 const ErrorMsgStyle = styled.div`
-  box-shadow: 2px 2px 5px
-  ${({ bcolor }) => (bcolor ? colorNames[bcolor] : primary)};
   padding: 5px 15px;
   width: 100%;
   text-align: center;
