2eaab8824f1982a5e18e2a3bf0128d6ae15a4e3f
ddoonsim <2sujg97@naver.com>
2024-02-14 17:51:11 +0900

    ㅋㅋㅋㅋㅋㅋ 세 번째 에러메세지 스타일 바꾸는 중..ㅎ

--- src/components/commons/ErrorMsgStyle.js
+++ src/components/commons/ErrorMsgStyle.js
@@ -2,17 +2,14 @@ import styled from 'styled-components';
 import colorNames from '../../styles/colors';
 import sizeNames from '../../styles/sizes';
 
-const { primary, info } = colorNames;
+const { info } = colorNames;
 const { small } = sizeNames;
 
 const Message = styled.div`
-  box-shadow: 2px 2px 5px
-    ${({ bcolor }) => (bcolor ? colorNames[bcolor] : primary)};
   padding: 5px 15px;
   width: 100%;
   text-align: center;
-  color: ${({ fcolor }) => (fcolor ? colorNames[fcolor] : info)};
-  font-size: ${small};
+  color: #BF0F0F;
   border-radius: 3px;
   margin: 5px 0;
 `;
