b2e6695ada86805fe68fa1bc369ca26b9b6289cb
ddoonsim <2sujg97@naver.com>
2024-02-15 19:17:13 +0900

    인증코드 이메일 전송 전, 중복 체크 추가

--- src/api/commons/sendEmail.js
+++ src/api/commons/sendEmail.js
@@ -1,15 +1,24 @@
 import apiRequest from '../../lib/apiRequest';
 
-export default function sendEmail(email) {
+// 인증 코드 이메일 발송
+export const sendVerifyEmail = function (email) {
   return new Promise((resolve, reject) => {
     apiRequest(`/email/verify?email=${email}`, 'GET')
       .then((res) => {
         console.log(res);
-        // if (typeof callbackEmailVerify == 'function') {
-        //   // 이메일 승인 코드 메일 전송 완료 후 처리 콜백
-        //   // callbackEmailVerify(data);
-        // }
+
+        alert('인증코드를 발송했습니다! 이메일을 확인하세요😊');
+        // 이메일 승인 코드 메일 전송 완료 후 처리 콜백
       })
       .catch((err) => console.error(err));
   });
-}
+};
+
+// 인증코드 일치 여부 확인
+export const sendEmailVerifyCheck = function (authNum) {
+  return new Promise(() => {
+    apiRequest(`/email/auth_check?authNum=${authNum}`, 'GET')
+      .then((data) => {})
+      .catch((err) => console.error(err));
+  });
+};
