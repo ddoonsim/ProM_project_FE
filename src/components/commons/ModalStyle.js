37e3995460ffbc8fcd833394e19b7efe81f9dc20
ddoonsim <2sujg97@naver.com>
2024-02-21 15:14:57 +0900

    ,,

--- src/components/commons/ModalStyle.js
+++ src/components/commons/ModalStyle.js
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-import colorNames from '../../styles/colors';
-import sizeNames from '../../styles/sizes';
 
 export const ModalStyle = styled.div`
   width: 300px;
