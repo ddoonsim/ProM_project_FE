2d548ea94bf6dc353a379c5fdb059f258776ae35
ShinJaeMin98 <jaemin6292@gmail.com>
2024-02-23 16:44:09 +0900

    회원정보 찾기 수정중

--- src/containers/member/FindIdContainer.js
+++ src/containers/member/FindIdContainer.js
@@ -5,71 +5,65 @@ import { findIdInfo } from '../../api/member/findid';
 import FindIdForm from '../../components/member/FindIdForm';
 
 const FindIdContainer = () => {
-    const { t } = useTranslation();
-    const navigate = useNavigate();
-    const [form, setForm] = useState({
-      agree: false,
-    });
-  
-    const [errors, setErrors] = useState({});
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    agree: false,
+  });
 
-    const onSubmit = useCallback(
-        (e) => {
+  const [errors, setErrors] = useState({});
 
-            e.preventDefault();
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-            let hasError = false; // 검증 실패 여부
-            const _errors = {};
-            setErrors(() => _errors);
-    
-          /* 필수 항목 검증 S */
-          const requiredFields = {
-            name: t('NotBlank_name'),
-            mobile: t('NotBlank_mobile'),
-          };
+      let hasError = false; // 검증 실패 여부
+      const _errors = {};
+      setErrors(() => _errors);
 
-          for (const field in requiredFields) {
-            _errors[field] = _errors[field] || [];
-            if (!form[field] || !form[field].trim()) {
-              _errors[field].push(requiredFields[field]);
-              hasError = true;
-            }
+      /* 필수 항목 검증 S */
+      const requiredFields = {
+        name: t('NotBlank_name'),
+        mobile: t('NotBlank_mobile'),
+      };
+
+      for (const field in requiredFields) {
+        _errors[field] = _errors[field] || [];
+        if (!form[field] || !form[field].trim()) {
+          _errors[field].push(requiredFields[field]);
+          hasError = true;
+        }
+      }
+      /* 필수 항목 검증 E */
+
+      if (hasError) {
+        setErrors(() => _errors);
+        return;
+      }
+
+      console.log('버튼 클릭!!');
+
+      // 회원정보 일치 확인
+      findIdInfo(form)
+        .then((res) => {
+          console.log(res.data);
+          if (res.success) {
+            // 회원정보 일치시 처리
+            // 가입한 아이디(이메일)가 모달창 또는 알림으로 나오도록 처리
+            alert("아이디 : " + res.data.replace(res.data.substr(2,4), "***"));
+
+            // 로그인 페이지 이동
+            navigate('/login', { replace: true });
+          } else {
+            alert('등록된 회원이 아님'); // 안나와,,,
           }
-          /* 필수 항목 검증 E */
-    
-          if (hasError) {
-            setErrors(() => _errors);
-            return;
-          }
+        })
+        .catch((err) => setErrors(() => err.message));
+    },
+    [form, t, navigate],
+  );
 
-          console.log('버튼 클릭!!');
-
-        // 회원정보 일치 확인
-         findIdInfo(form)
-         .then((res) => {
-            console.log(res.success);
-            if (res.success) {
-                
-             // 회원정보 일치시 처리
-             // 가입한 아이디(이메일)가 모달창 또는 알림으로 나오도록 처리
-             alert('회원정보 일치시 처리');
-
-             // 로그인 페이지 이동
-             navigate('/login', { replace: true });
-
-            } else {
-              alert('등록된 회원이 아님');  // 안나와,,,
-            }
-
-         })
-          .catch((err) => setErrors(() => err.message));
-      
-        },
-        [form, t, navigate],
-    );
-    
-
-    // OnChange 함수
+  // OnChange 함수
   const onChange = useCallback((e) => {
     const target = e.currentTarget;
 
@@ -78,11 +72,10 @@ const FindIdContainer = () => {
     }
 
     setForm((form) => ({
-        ...form,
-        [target.name]: target.value,
+      ...form,
+      [target.name]: target.value,
     }));
-
-    }, []);
+  }, []);
 
   // 자동 하이픈 추가 함수
   const autoHyphen = (target) => {
@@ -107,15 +100,14 @@ const FindIdContainer = () => {
 
     target.value = formattedPhone;
   };
-    
 
-      return (
-        <FindIdForm 
-        onChange={onChange} 
-        onSubmit={onSubmit}
-        form={form} 
-        errors={errors}
-      />
-      );
+  return (
+    <FindIdForm
+      onChange={onChange}
+      onSubmit={onSubmit}
+      form={form}
+      errors={errors}
+    />
+  );
 };
-    export default FindIdContainer;
+export default FindIdContainer;
