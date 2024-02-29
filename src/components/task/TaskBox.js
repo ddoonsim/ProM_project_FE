ffc71e382e47756e4907aeb438cd0dbb5a878ad1
easy-0 <lc_dyna@naver.com>
2024-02-29 18:01:38 +0900

    구조 변경

--- src/components/task/TaskBox.js
+++ src/components/task/TaskBox.js
@@ -1,97 +1,31 @@
-import React, { useState, useCallback, useEffect, useReducer } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback, useState } from 'react';
 import ModalBox from '../commons/ModalBox';
-import styled from 'styled-components';
-import colorNames from '../../styles/colors';
-import sizeNames from '../../styles/sizes';
-import TaskDetail from '../../pages/front/task/TaskDetail';
-import AddTask from '../../pages/front/project/AddTask';
-import { getTask } from '../../api/task/TaskInfo';
-
-// 초기 상태
-const initialState = {
-  taskList: [], // 작업 목록
-  loading: true, // 데이터 로딩 상태
-};
-
-// 액션 타입
-const actionTypes = {
-  SET_TASK_LIST: 'SET_TASK_LIST',
-  SET_LOADING: 'SET_LOADING',
-};
-
-// 리듀서 함수
-const reducer = (state, action) => {
-  switch (action.type) {
-    case actionTypes.SET_TASK_LIST:
-      return {
-        ...state,
-        taskList: action.payload,
-        loading: false,
-      };
-    case actionTypes.SET_LOADING:
-      return {
-        ...state,
-        loading: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
-const TaskBox = ({ item, tasks, listStatus }) => {
-  // 리듀서를 사용하여 상태 관리
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [isOpen, setIsOpen] = useState(false);
-  const onClose = useCallback(() => setIsOpen(false), []);
-  const onClick = (seq) => {
-    console.log('업무 딸깍');
-    console.log(seq);
-    getTask(seq).then((item) => {
-      console.log(item);
-      setTask(item);
-    });
-  };
-  const [task, setTask] = useState({});
-
-  useEffect(() => {
-    if (Object.keys(tasks).length === 0) {
-      console.log('tasks loading...');
-      dispatch({ type: actionTypes.SET_LOADING, payload: true });
-    } else {
-      const filteredTasks = tasks.filter(({ status }) => status === listStatus);
-      dispatch({ type: actionTypes.SET_TASK_LIST, payload: filteredTasks });
-    }
-  }, [tasks, listStatus]);
-
+import TaskDetailContainer from '../../containers/task/TaskDetailContainer';
+const TaskBox = ({ tasks, listStatus }) => {
+  const [open, setOpen] = useState(false);
+  const [taskSeq, setTaskSeq] = useState(null);
+  const onClick = useCallback((seq) => {
+    setOpen(true);
+    setTaskSeq(seq);
+  }, []);
   return (
     <>
-      {state.loading ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          {state.taskList.map(({ seq, sdate, edate, tname }) => (
-            <>
-              <Link
-                onClick={() => {
-                  onClick(seq);
-                  setIsOpen(!isOpen);
-                }}
-              >
-                {tname}
-              </Link>
-              <hr />
-            </>
+      {tasks &&
+        tasks.length > 0 &&
+        tasks
+          .filter(({ status }) => status === listStatus.toUpperCase())
+          .map(({ seq, tname }) => (
+            <div key={seq} onClick={() => onClick(seq)}>
+              {tname}
+            </div>
           ))}
-        </div>
-      )}
-      {isOpen && (
-        <ModalBox isOpen={isOpen} onClose={onClose}>
-          <AddTask tasks={task} item={item} />
+      {open && (
+        <ModalBox isOpen={open}>
+          <TaskDetailContainer seq={taskSeq} />
         </ModalBox>
       )}
     </>
   );
 };
 
-export default TaskBox;
+export default React.memo(TaskBox);
