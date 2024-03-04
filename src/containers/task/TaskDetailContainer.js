92c450b48e13c9e5cda652af9b4b5dcc18706d60
easy-0 <lc_dyna@naver.com>
2024-03-04 21:11:12 +0900

    업무 추가 가능

--- src/containers/task/TaskDetailContainer.js
+++ src/containers/task/TaskDetailContainer.js
@@ -88,6 +88,7 @@ const TaskDetailContainer = ({ seq, pSeq, members }) => {
   }, [t, task]);
 
   const onChange = useCallback(
+    console.log(task),
     (e) =>
       setForm((form) => ({ ...form, [e.target.name]: e.target.value.trim() })),
     [],
@@ -187,7 +188,8 @@ const TaskDetailContainer = ({ seq, pSeq, members }) => {
       }
 
       saveTask(task, pSeq);
-      window.location.reload();
+      console.log('task========', task);
+      // window.location.reload();
     },
     [task, pSeq, validateTask],
   );
@@ -210,9 +212,11 @@ const TaskDetailContainer = ({ seq, pSeq, members }) => {
     [],
   );
   const onSelectMembers = useCallback((selectedOptions) => {
+    console.log('ssssss', selectedOptions);
     setTask(
       produce((draft) => {
         draft.memberSeqs = selectedOptions.map((m) => m.value).join(',');
+        draft.members = selectedOptions;
       }),
     );
 
@@ -244,40 +248,42 @@ const TaskDetailContainer = ({ seq, pSeq, members }) => {
   /* 프로젝트 구성 멤버 */
   /* 서브 태스크 E */
 
-    // 진행률
-    const percentTodo = () => {
-      const completedTodos = task.todos.filter((todo) => todo.done).length;
-      return task.todos.length ? (completedTodos / task.todos.length) * 100 : 0;
-    };
-  
-    // 폭죽 효과
-    const confettiConfig = {
-      angle: 90,
-      spread: 360,
-      startVelocity: 70,
-      elementCount: 800,
-      dragFriction: 0.05,
-      duration: 5000,
-      stagger: 1,
-      width: '30px',
-      height: '30px',
-      perspective: '500px',
-      colors: [
-        '#FFD700',
-        '#FF6347',
-        '#00FF00',
-        '#87CEEB',
-        '#FF00FF',
-        '#FFFF00',
-        '#00FFFF',
-        '#FFA07A',
-        '#FF1493',
-        '#00FF7F',
-      ],
-      opacity: 0.9,
-      origin: { x: 0.8, y: 0.2 },
-    };
+  // 진행률;
+  const percentTodo = () => {
+    if (!Object.values(task).seq) return;
+    const completedTodos = task.todos
+      ? task.todos.filter((todo) => todo.done).length
+      : 0;
+    return task.todos.length ? (completedTodos / task.todos.length) * 100 : 0;
+  };
 
+  // 폭죽 효과
+  const confettiConfig = {
+    angle: 90,
+    spread: 360,
+    startVelocity: 70,
+    elementCount: 800,
+    dragFriction: 0.05,
+    duration: 5000,
+    stagger: 1,
+    width: '30px',
+    height: '30px',
+    perspective: '500px',
+    colors: [
+      '#FFD700',
+      '#FF6347',
+      '#00FF00',
+      '#87CEEB',
+      '#FF00FF',
+      '#FFFF00',
+      '#00FFFF',
+      '#FFA07A',
+      '#FF1493',
+      '#00FF7F',
+    ],
+    opacity: 0.9,
+    origin: { x: 0.8, y: 0.2 },
+  };
 
   return (
     <MainBox>
@@ -305,35 +311,34 @@ const TaskDetailContainer = ({ seq, pSeq, members }) => {
           form={form}
         />
         <div>
-        <div
-          style={{
-            marginTop: '10px',
-            marginBottom: '10px',
-            paddingLeft: '10px',
-          }}
-        >
-        </div>
-        <div
-          style={{
-            width: '100%',
-            height: '12px',
-            background: '#f3f3f3',
-            borderRadius: '6px',
-            overflow: 'hidden',
-          }}
-        >
           <div
             style={{
-              width: `${percentTodo()}%`,
-              height: '100%',
-              background:
-                'linear-gradient(to right, #0A4B59 ,#3E768C, #88B0BF)',
-              borderRadius: '6px',
-              transition: 'width 0.5s ease',
+              marginTop: '10px',
+              marginBottom: '10px',
+              paddingLeft: '10px',
             }}
-          />
+          ></div>
+          <div
+            style={{
+              width: '100%',
+              height: '12px',
+              background: '#f3f3f3',
+              borderRadius: '6px',
+              overflow: 'hidden',
+            }}
+          >
+            <div
+              style={{
+                width: `${percentTodo()}%`,
+                height: '100%',
+                background:
+                  'linear-gradient(to right, #0A4B59 ,#3E768C, #88B0BF)',
+                borderRadius: '6px',
+                transition: 'width 0.5s ease',
+              }}
+            />
+          </div>
         </div>
-      </div>
         <TodoList
           todos={(task && task.todos) || []}
           onToggle={onTodoToggle}
